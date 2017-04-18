b8c6af52f483d9da41d88f9f010524eabeac9e5c
Joshua Choo <joshuachoo.jc@gmail.com>
2017-04-18 23:21:01 +0800

    msm8996-common: Power-HAL: Make it tunable

--- power/Android.mk
+++ power/Android.mk
@@ -24,7 +24,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_MODULE := android.hardware.power@1.1-service.gemini
 LOCAL_INIT_RC := android.hardware.power@1.1-service.gemini.rc
-LOCAL_SRC_FILES := service.cpp Power.cpp power-helper.c metadata-parser.c utils.c list.c hint-data.c
+LOCAL_SRC_FILES := service.cpp Power.cpp power-helper.c metadata-parser.c utils.c list.c hint-data.c power-common.c
 
 # Include target-specific files.
 ifeq ($(call is-board-platform-in-list, msm8996), true)

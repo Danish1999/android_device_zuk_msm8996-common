507171d3a67b9ce01147ce60661a67681cdd8d98
Danish1999 <danish01031999@gmail.com>
2018-12-04 07:03:14 +0000

    msm8996-common: Inlude HWUI values from fw/native

--- msm8996.mk
+++ msm8996.mk
@@ -42,6 +42,9 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
+# HWUI and Dalvik VM overrides
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-dalvik-heap.mk)
+
 # Permissions
 PRODUCT_COPY_FILES += \
     external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml \
